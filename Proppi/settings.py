d27498cb43007529f8adc60d8bbd5b6391572c1f
Fabri <bellinviafab@gmail.com>
2024-10-05 19:21:41 -0300

    Commit

--- Proppi/settings.py
+++ Proppi/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = 'django-insecure-+99gsyb%^p+pc-v^iqg+%f2)^%hwrt2_=+0uew_ygj5lpx3+az
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1.nip.io' , '127.0.0.1' , 'ljn1lk6f-8000.brs.devtunnels.ms/' , 'localhost' , '6d64-2800-2265-9000-d74-8840-1902-e3d7-1ea6.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1.nip.io' , '127.0.0.1' , 'ljn1lk6f-8000.brs.devtunnels.ms/' , 'localhost' , '70c2-190-191-1-145.ngrok-free.app']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://6d64-2800-2265-9000-d74-8840-1902-e3d7-1ea6.ngrok-free.app',  # tu URL de ngrok
+    'https://70c2-190-191-1-145.ngrok-free.app',  # tu URL de ngrok
 ]
 
 # Application definition
